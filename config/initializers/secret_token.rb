8b8f8a889692df675a2cd9a91332e08d865a2308
Ashish Virmani <virmani.ashish@gmail.com>
2011-11-14 22:44:13 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LocationsApp::Application.config.secret_token = 'e70193256712f1e330236cdba3495778c79b706210c440ece379b1b19b86b08d71ea7e183cee950a75460da996fe33ec9fde597442a839e53303507af582d0cc'
