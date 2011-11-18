23c4c38dd88e7078ca60ef4298b33b8f0af9ebc3
Ashish Virmani <virmani.ashish@gmail.com>
2011-11-17 22:34:04 -0800

    Added validations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,5 @@
 class Location < ActiveRecord::Base
+  validates_uniqueness_of :name
+  validates_numericality_of :latitude
+  validates_numericality_of :longitude
 end
--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -1,2 +1,3 @@
 class Statistic < ActiveRecord::Base
+    validates_uniqueness_of :status_code
 end
