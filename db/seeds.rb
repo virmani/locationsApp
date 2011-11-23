dc80ea1bfa854f7a4f30fc3321caa80940a63a76
Ashish Virmani <virmani.ashish@gmail.com>
2011-11-22 18:28:16 -0800

    Fixing the seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Statistic.create([{status_code: '1', count: 0, average: 0.0, variance: 0.0}, Statistic.create([{status_code: '2', count: 0, average: 0.0, variance: 0.0}, {status_code: '3', count: 0, average: 0.0, variance: 0.0}, {status_code: '4', count: 0, average: 0.0, variance: 0.0}, {status_code: '5', count: 0, average: 0.0, variance: 0.0}])
+Statistic.create([{status_code: '1', count: 0, average: 0.0, variance: 0.0}, {status_code: '2', count: 0, average: 0.0, variance: 0.0}, {status_code: '3', count: 0, average: 0.0, variance: 0.0}, {status_code: '4', count: 0, average: 0.0, variance: 0.0}, {status_code: '5', count: 0, average: 0.0, variance: 0.0}])
 
